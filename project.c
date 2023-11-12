b613bc85d7e1acbed04fa5f5d2371ddfdd2a6e6c
Antonia <antoniamagdau@gmail.com>
2023-11-12 20:47:22 +0200

    adding needed fields for files

--- project.c
+++ project.c
@@ -5,6 +5,150 @@
 #include<dirent.h>
 #include<string.h>
 #include<sys/stat.h>
+#include<time.h>
+void cerinte(char *path,char* name, struct stat *st_file)
+{
+  char str[3000];
+  char buff[100];//buffer pt transformarea val din intregi in sir de caractere
+  
+  //adaugare nume
+  if(S_ISREG(st_file->st_mode) || (S_ISREG(st_file->st_mode) && strstr(path,".bmp")))
+    strcpy(str,"nume fisier: ");
+  if(S_ISLNK(st_file->st_mode))
+    strcpy(str,"nume legatura: ");
+  if(S_ISDIR(st_file->st_mode))
+      strcpy(str,"nume director: ");
+  
+  strcat(str,name);
+  strcat(str,"\n");
+
+  //adaugare dimensiuni pentru fisierele bmp
+  if(S_ISREG(st_file->st_mode)&& strstr(path,".bmp"))
+    {
+      int fd;
+      fd=open(path,O_RDONLY);
+      if(fd==-1)
+	{
+	  perror("error.open");
+	  exit(1);
+	}
+      int lungime,inaltime;
+      lseek(fd,18,SEEK_SET);
+      if(read(fd,&inaltime,sizeof(int))!=sizeof(int))
+	{
+	  perror("error.read");
+	  if(close(fd)==-1){
+	    perror("error.close");
+	    exit(1);
+	  }
+	  exit(1);
+	}
+      if(read(fd,&lungime,sizeof(int))!=sizeof(int))
+	{
+	  perror("error.read");
+	  if(close(fd)==-1){
+	    perror("error.close");
+	    exit(1);
+	  }
+	  exit(1);
+	}
+      
+      strcat(str,"inaltime: ");
+      sprintf(buff,"%d\n",inaltime);
+      strcat(str,buff);
+      strcat(str,"lungime: ");
+      sprintf(buff,"%d\n",lungime);
+      strcat(str,buff);
+      if(close(fd)==-1)
+	{
+	  perror("error close poza");
+	  exit(1);
+	}
+    }
+  
+  //dimensiunea pentru de legatura simbolica
+  if(S_ISLNK(st_file->st_mode))
+    {
+      struct stat st_link;
+      if(lstat(path,&st_link) == -1)
+	{
+	  perror("stat error");
+	  exit(1);
+	}
+      sprintf(buff,"dimensiune legatura: %ld\n",st_link.st_size);
+      strcat(str,buff);
+    }
+  
+  //dimensiune fisier pt fisier regulat,bmp sau legatura simbolica 
+  if(S_ISREG(st_file->st_mode) || (S_ISREG(st_file->st_mode)&& strstr(path,".bmp")) || S_ISLNK(st_file->st_mode))
+    {
+      strcat(str,"dimensiune: ");
+      sprintf(buff,"%ld\n",st_file->st_size);
+      strcat(str,buff);
+    }
+  
+  //ID utilizator pt fisier regulat,bmp sau director
+  if(S_ISREG(st_file->st_mode) || (S_ISREG(st_file->st_mode)&& strstr(path,".bmp")) || S_ISDIR(st_file->st_mode))
+    {
+      strcat(str,"identificatorul utilizatorului: ");
+      sprintf(buff,"%d\n",st_file->st_uid);
+      strcat(str,buff);
+    }
+  
+  //timpul ultimei modificari pt fisier bmp sau regulat
+  if(S_ISREG(st_file->st_mode) || (S_ISREG(st_file->st_mode)&& strstr(path,".bmp")))
+    {
+      strcat(str,"timpul ultimei modificari: ");
+      strcat(str,ctime(&st_file->st_mtime));
+    }
+  
+  //contorul de legaturi pt fisier bmp sau regulat
+  if(S_ISREG(st_file->st_mode) || (S_ISREG(st_file->st_mode)&& strstr(path,".bmp")))
+  {
+    strcat(str,"contorul de legaturi: ");
+    sprintf(buff,"%ld\n",st_file->st_nlink);
+    strcat(str,buff);
+  }
+  
+  //drepturi de acces user
+  //drepturi de acces grup
+  //drepturi de acces altii
+  strcat(str,"drepturi de acces user: ");
+  sprintf(buff,"%s%s%s\n", st_file->st_mode & S_IRUSR ? "R" : "-",st_file->st_mode & S_IWUSR ? "W" : "-",st_file->st_mode & S_IXUSR ? "X" : "-");
+  strcat(str,buff);
+  strcat(str,"drepturi de acces grup: ");
+  sprintf(buff,"%s%s%s\n", st_file->st_mode & S_IRGRP ? "R" : "-",st_file->st_mode & S_IWGRP ? "W" : "-",st_file->st_mode & S_IXGRP ? "X" : "-");
+  strcat(str,buff);
+  strcat(str,"drepturi de acces altii: ");
+  sprintf(buff,"%s%s%s\n",st_file->st_mode & S_IROTH ? "R" : "-",st_file->st_mode & S_IWOTH ? "W" : "-",st_file->st_mode & S_IXOTH ? "X" : "-");
+  strcat(str,buff);
+  strcat(str,"\n");
+
+  
+  char *output="statistica.txt";
+  struct stat st_output;
+  if(stat(output,&st_output)==-1){
+    perror("stat error");
+    exit(1);
+  }
+  int file_fd1;
+  file_fd1=open(output,O_WRONLY | O_APPEND);
+  if(file_fd1==-1)
+    {
+      perror("error open file");
+      exit(1);
+    }
+  if(write(file_fd1,str,strlen(str))==-1)
+    {
+      perror("error write file");
+      exit(1);
+    }
+  if(close(file_fd1)==-1)
+    {
+      perror("error close output file");
+      exit(1);
+    }
+}
 void citire_director(char *director)
 {
   DIR *dir;
@@ -15,93 +159,28 @@ void citire_director(char *director)
       exit(1);
     }
 
-  struct dirent *entry;
-  struct stat file;
-  char path[300];
+  struct dirent *entry=NULL;
+  
   while((entry=readdir(dir))!=NULL)
     {
       if(strcmp(entry->d_name,".")!=0 && strcmp(entry->d_name,"..")!=0)
 	{
+	  struct stat file;
+	  char path[300]={};
 	  sprintf(path,"%s/%s",director,entry->d_name);
-
+	  
 	  if(stat(path,&file)==-1)
 	    {
 	      perror("error stat");
 	      exit(1);
 	    }
-	  if(S_ISREG(file.st_mode))
+	  if(S_ISDIR(file.st_mode))
 	    {
-	      if(strstr(path,".bmp"))
-		{
-		  
-		  int fd;
-		  fd=open(path,O_RDONLY);
-		  if(fd==-1)
-		    {
-		      perror("error.open");
-		      exit(1);
-		    }
-		  int lungime,inaltime;
-		  lseek(fd,18,SEEK_SET);
-		  if(read(fd,&inaltime,sizeof(int))!=sizeof(int))
-		    {
-		      perror("error.close");
-		      if(close(fd)==-1){
-			perror("error.open");
-			exit(1);
-		      }
-		      exit(1);
-		    }
-		  if(read(fd,&lungime,sizeof(int))!=sizeof(int))
-		    {
-		      perror("error.close");
-		      if(close(fd)==-1){
-			perror("error.open");
-			exit(1);
-		      }
-		      exit(1);
-		    }
-		  printf("lungime %d\n",lungime);
-		  printf("inaltime %d\n",inaltime);
-		  //
-		  char str1[500];
-		  char *output="statistica.txt";
-		  struct stat st_output;
-		  if(stat(output,&st_output)==-1){
-		    perror("stat error");
-		    exit(1);
-		  }
-		  int file_fd1;
-		  file_fd1=open(output,O_RDWR);
-		  if( file_fd1 == -1 )
-		    {
-		      perror("error open file");
-		      exit(1);
-		    }
-		  
-		  sprintf(str1,"nume fisier:%s\ninaltime: %d\nlungime %d\ndimensiune:%ld",entry->d_name,inaltime,lungime,(&file)->st_size);
-
-		  if(write(file_fd1,str1,strlen(str1))==-1)
-		    {
-		      perror("error write file");
-		      exit(1);
-		    }
-		  if(close(file_fd1)==-1)
-		    {
-		      perror("error close file2");
-		      exit(1);
-		    }
-		  ///
-		  if(close(fd)==-1){
-		    perror("error.open");
-		    exit(1);
-		  }
-		  
-		}
-	      else
-		printf("regular\n");
+	      cerinte(path, entry->d_name, &file);
+	      citire_director(path);
 	    }
-	  printf("%s\n",path);
+	  else
+	      cerinte(path,entry->d_name,&file);
 	}
     }
 
@@ -113,13 +192,13 @@ void citire_director(char *director)
 
 int main(int argc,char*argv[])
 {
-if(argc!=2)
-{
-perror("incorect number of args");
-exit(1);
-}
-citire_director(argv[1]);
-return 0;
+  if(argc!=2)
+    {
+      perror("incorect number of args");
+      exit(1);
+    }
+  citire_director(argv[1]);
+  return 0;
 }
 //fol structura stat pt fiecare fis, verif cu st_mode daca e regulat,legat simbolica sau director si afisam atr pt fiecare fis, trebuie ignorate cele doua .. si . cu un strcmp prob
 //stat trb sa primeasca intreaga cale carte fisier cu sprintf director/numefisier
